cb7b8f17e2969d550035603369c072072f9d4f84
Morgan Bourgeois <19302449+cladsam@users.noreply.github.com>
2024-01-05 20:34:57 +0100

    add game function first complete game version

--- javascript.js
+++ javascript.js
@@ -46,4 +46,36 @@ function normalizeSelection(playerSelection) {
     return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
 };
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    let tour = 1;
 
+    do {
+        // Play one round    
+        let userChoice = prompt("Rock, Paper or Scissor ?");
+        const computerChoice = getComputerChoice();
+        const turnResult = playRound(userChoice, computerChoice);
+
+        // display round result    
+        switch (turnResult) {
+            case gameResult.Tie:
+                console.log("It's a Tie !");;
+                break;
+            case gameResult.PlayerWin:
+                console.log("You win " + userChoice + "beats " + computerChoice);
+                scorePlayer++;
+                break;
+            case gameResult.PlayerLoose:
+                console.log("You loose " + computerChoice + "beats " + userChoice);
+                scoreComputer++;
+                break
+        }
+
+    }
+    while (scoreComputer < 5 && scorePlayer < 5)
+
+    // display Game result
+    scoreComputer > scorePlayer ? console.log("You loss " + scoreComputer + "to " + scorePlayer) : console.log("You won " + scorePlayer + "to " + scoreComputer);
+
+}
